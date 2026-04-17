6084876f861abffb9bfa511ffb38591cd36efad1
Marcel Koseler <mkoseler@MarcelWindows.home>
2026-04-17 12:50:09 +0200

    add some more software

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux nvim alacritty fuzzel
 dnf5 copr -y enable avengemedia/dms
 dnf5 install -y niri dms
 
